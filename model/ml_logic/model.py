f564ae425f2ab07abcfdf79f84d71322a3010885
Thibault Bellaton <thibault.bellaton98@gmail.com>
2023-09-07 02:53:09 +0200

    first part of the packaging with data model and preprocessing

--- model/ml_logic/model.py
+++ model/ml_logic/model.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+from xgboost import XGBClassifier
 
 '''
 Here is where we will initialize and train the model
@@ -8,11 +10,22 @@ def initialize_model() -> Model:
     '''
     Add our baseline or our final model with the correct parameters here
     '''
-    pass # ADD CODE HERE
+    model = XGBClassifier(colsample_bytre = 0.8,
+                          learning_rate = 0.01,
+                          max_depth = 4,
+                          n_estimators = 100,
+                          subsample = 0.8)
+    print("✅ Model initialized")
 
-def train_model():
+    return model
+
+def train_model(model: Model,
+        X: np.ndarray,
+        y: np.ndarray):
     '''
     Train the model with this function. Training data should
     be cleaned and engineered and preprocessed.
     '''
-    pass # ADD CODE HERE
+    model = model.fit(X, y)
+    
+    return model
