933ac8fb4ba5d4743c16ec3a4b58b9ee77f605ad
patrick evans <patrickevans29@gmail.com>
2023-09-12 19:52:01 +0200

    final model upload to google container registry

--- ipl_model/params.py
+++ ipl_model/params.py
@@ -2,8 +2,10 @@ import os
 
 #####CONSTANTS#####
 LOCAL_DATA_PATH = os.path.join(os.path.expanduser('~'), "code", "ipl_prediction_model", "raw_data")
-LOCAL_MODELS_PATH =  os.path.join(os.path.expanduser('~'), "code", "ipl_prediction_model", "model")
-LOCAL_PREPROCESSORS_PATH = os.path.join(os.path.expanduser('~'), "code", "ipl_prediction_model", "preprocessor")
+LOCAL_MODELS_PATH =  os.path.join(os.getcwd(), "ipl_model", "model")
+LOCAL_PREPROCESSORS_PATH = os.path.join(os.getcwd(), "ipl_model", "preprocessor")
+DOCKER_MODEL_PATH = os.path.join(os.path.expanduser('~'), "ipl_model", "model")
+DOCKER_PREPROCESSOR_PATH = os.path.join(os.path.expanduser('~'), "ipl_model", "preprocessor")
 
 ##################  VARIABLES  ##################
 BUCKET_NAME = os.environ.get("BUCKET_NAME") # Make sure the env file is made
@@ -20,21 +22,21 @@ PLAYER_PROCESSING_COLUMNS = ['ID', 'innings', 'overs', 'ballnumber', 'batter',
 WIN_LOSS_PROCESSING_COLUMNS = ['ID', 'Season', 'Team1', 'Team2', 'WinningTeam',
                              'Player_of_Match', 'Team1Players', 'Team2Players']
 
-COLUMN_NAMES = ['City', 'Season', 'Team1', 'Team2', 'Venue', 'TossWinner',
+COLUMN_NAMES = ['City', 'Team1', 'Team2', 'Venue', 'TossWinner',
        'TossDecision', 'WinningTeam', 'Avg_Weighted_Score_diff',
        'batting_average_weighted_diff', 'batting_strike_rate_weighted_diff',
        'bowling_average_diff', 'bowling_economy_rate_diff', 'win_ratio_diff']
 
 TARGET_NAME = ['WinningTeam']
 
-FEATURE_NAMES = ['City', 'Season', 'Team1', 'Team2', 'Venue', 'TossWinner',
+FEATURE_NAMES = ['City', 'Team1', 'Team2', 'Venue', 'TossWinner',
        'TossDecision', 'Avg_Weighted_Score_diff',
        'batting_average_weighted_diff', 'batting_strike_rate_weighted_diff',
        'bowling_average_diff', 'bowling_economy_rate_diff', 'win_ratio_diff']
 
 CATAGORICAL_COLUMNS = ['Team1', 'Team2', 'Venue', 'TossDecision', 'City']
 
-NUMERICAL_COLUMNS = ['TossWinner', 'TossDecision', 'Avg_Weighted_Score_diff',
+NUMERICAL_COLUMNS = ['TossWinner', 'Avg_Weighted_Score_diff',
        'batting_average_weighted_diff', 'batting_strike_rate_weighted_diff',
        'bowling_average_diff', 'bowling_economy_rate_diff', 'win_ratio_diff']
 
